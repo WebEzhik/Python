fbffebc3e17f5759cf76179360d11a298c42cd52
Kulakov Anton <kulakov_3000@mail.ru>
2022-10-18 11:59:33 +0300

    21

--- Home_work_3.py
+++ Home_work_3.py
@@ -15,22 +15,37 @@
 # print(sum)
 
 #Задача 2
-import random
+# import random
 
-#N = int(input('Введите размер списка: '))
-N = 4
+# #N = int(input('Введите размер списка: '))
+# N = 5
 
-list1 = []
-for i in range(N):
-    list1.append(random.randint(0, 10))
-print(list1)
+# list1 = []
+# for i in range(N):
+#     list1.append(random.randint(0, 10))
+# print(list1)
 
-mult = []
+# mult = []
+# i = 0
+# while i <= N - i - 1:
+#     mult.append(list1[i] * list1[N - i - 1])
+#     i = i + 1
 
-j = 0
-while j >= N - j - 1:
-    mult.append(list1[j] * list1[N - j - 1])
-    j =+ 1
+# print(mult)
 
+#Задача 3
+list = [1.1, 1.2, 3.1, 5, 10.01]
+print(list)
 
-print(mult)
+min = round(list[0] % 1, 2)
+max = round(list[0] % 1, 2)
+for i in list:
+    print(max)
+    if 
+    if max < i % 1:
+        max = round(i % 1, 2)
+    if min > i % 1:
+        min = round(i % 1, 2)
+print(min)
+print(max)
+print(max-min)
