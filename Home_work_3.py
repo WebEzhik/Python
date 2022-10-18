dac4aa79750633812a051b6322f3397aa2e23605
Kulakov Anton <kulakov_3000@mail.ru>
2022-10-18 20:30:07 +0300

    Домашняя работа 3

--- Home_work_3.py
+++ Home_work_3.py
@@ -1,4 +1,4 @@
-#Задача 1
+# Задача 1
 # import random
 
 # N = int(input('Введите размер списка: '))
@@ -14,7 +14,7 @@
 #     #print(list1[i])
 # print(sum)
 
-#Задача 2
+# Задача 2
 # import random
 
 # #N = int(input('Введите размер списка: '))
@@ -33,19 +33,52 @@
 
 # print(mult)
 
-#Задача 3
-list = [1.1, 1.2, 3.1, 5, 10.01]
-print(list)
+# Задача 3
+# list = [1.1, 1.2, 3.1, 5, 10.01]
+# print(list)
 
-min = round(list[0] % 1, 2)
-max = round(list[0] % 1, 2)
-for i in list:
-    print(max)
-    if 
-    if max < i % 1:
-        max = round(i % 1, 2)
-    if min > i % 1:
-        min = round(i % 1, 2)
-print(min)
-print(max)
-print(max-min)
+# min = round(list[0] % 1, 5)
+# max = round(list[0] % 1, 5)
+# for i in list:
+#     #print(round(i%1, 5))
+#     if(round(i%1, 5) != 0):
+#         if max < i % 1:
+#             max = round(i % 1, 5)
+#         if min > i % 1:
+#             min = round(i % 1, 5)
+
+# # print()
+# # print("min", min)
+# # print("min", max)
+# print(max-min)
+
+
+# Задача 3
+
+# N = int(input('Введите десятичное число: '))
+
+# b = ''
+
+# while N > 0:
+#     b = str(N % 2) + b
+#     N = N // 2
+
+# print(b)
+
+# Задача 4
+import math
+#N = int(input('Введите число: '))
+N = 8
+
+Fib =[0] * (N * 2 +1)
+
+Fib[N + 1] = 1
+Fib[N - 1] = Fib[N + 1]
+
+for i in range(2, N + 1):
+    Fib[N + i] = Fib[N + i - 2] + Fib[N + i - 1]
+    Fib[N - i] = Fib[N - i + 2] - Fib[N - i + 1]
+
+print(Fib)
+
+
